1d381559e1549b7ed6441a407ace1854f7797044
Mateus Cardoso <serjumano17@gmail.com>
2022-04-14 17:28:26 -0300

    feat!: basic use of puppeteer in AWS lambda

--- handler.js
+++ handler.js
@@ -1,20 +1,50 @@
 'use strict';
+const chromium = require('chrome-aws-lambda');
+const { addExtra } = require('puppeteer-extra')
+
+async function getDownDetectorInformations(serviceName) {
+  const puppeteerExtra = addExtra(chromium.puppeteer)
+  const browser = await puppeteerExtra
+    .launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless
+    })
+
+  const page = await browser.newPage()
+  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
+  page.setDefaultNavigationTimeout(0)
+
+  const url = `https://downdetector.com.br/fora-do-ar/${serviceName}`
+  const response = await page.goto(url, { waitUntil: 'domcontentloaded' })
+    .then(response => response.status())
+    .catch(error => 403)
+
+  return response
+}
 
 module.exports.hello = async (event) => {
-  const params = event.pathParameters
-  const name = params.serviceName || 'Not found'
+  try {
+    const params = event.pathParameters
+    const name = params.serviceName || 'Not found'
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Go Serverless v1.0! Your function executed successfully! Service:  ${name}`,
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    const response = await getDownDetectorInformations(name)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: `Go Serverless v1.0! Your function executed successfully! Service:  ${name}, Status: ${response}`,
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    return error
+  }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
