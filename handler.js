3d3d881fc1cfa05c72b6daee35dd5bfbd50808f3
Mateus Cardoso <serjumano17@gmail.com>
2022-04-18 08:08:09 -0300

    refactor!: content format

--- handler.js
+++ handler.js
@@ -1,8 +1,27 @@
 'use strict';
 const chromium = require('chrome-aws-lambda');
 const { addExtra } = require('puppeteer-extra')
+const moment = require('moment-timezone')
+
+function normalizeDownDetectorResult(downDetectorResult) {
+  const baselines = downDetectorResult.baseline
+  const reports = downDetectorResult.reports
+
+  const data = baselines.map((baseline, index) => {
+    return {
+      
+      date: moment(baseline.x).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss'),
+      baseline: baseline.y,
+      notificationCount: reports[index].y
+    }
+  })
+
+  return data
+}
 
 async function getDownDetectorInformations(serviceName) {
+  let response = {}
+  
   const puppeteerExtra = addExtra(chromium.puppeteer)
   const browser = await puppeteerExtra
     .launch({
@@ -17,11 +36,59 @@ async function getDownDetectorInformations(serviceName) {
   page.setDefaultNavigationTimeout(0)
 
   const url = `https://downdetector.com.br/fora-do-ar/${serviceName}`
-  const response = await page.goto(url, { waitUntil: 'domcontentloaded' })
+  const status = await page.goto(url, { waitUntil: 'domcontentloaded' })
     .then(response => response.status())
     .catch(error => 403)
 
-  return response
+  if (status === 200) {
+    const result = await page.evaluate(() => {
+      const titleElement = document.getElementsByClassName('entry-title')[0]
+      const titleTextContent = String(titleElement.textContent)
+      
+      // get title
+      const title = titleTextContent.replace('\n', '').replace('\t', '').replace('\r', '').trim()	
+
+      const currentServiceProperties = window['DD']['currentServiceProperties']
+      // string
+      const status = currentServiceProperties['status']
+      const series = currentServiceProperties['series']
+
+      // array 
+      //   x: string,
+      //   y: string,
+      const baseline = series['baseline']['data']
+      // array 
+      //   x: string,
+      //   y: string,
+      const reports = series['reports']['data']
+
+      return {
+        title,
+        status,
+        baseline,
+        reports
+      }
+    })
+
+    console.log(result)
+
+    const normalizeResult = normalizeDownDetectorResult(result)
+
+    console.log(normalizeResult)
+    response = {
+      url,
+      status: result.status,
+      title: result.title,
+      content: normalizeResult
+    }
+  }
+
+  await browser.close()
+
+  return {
+    status,
+    result: response
+  }
 }
 
 module.exports.hello = async (event) => {
@@ -30,16 +97,15 @@ module.exports.hello = async (event) => {
     const name = params.serviceName || 'Not found'
 
     const response = await getDownDetectorInformations(name)
+    console.log(`resposta do servidor: ${response.status}`)
 
     return {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: `Go Serverless v1.0! Your function executed successfully! Service:  ${name}, Status: ${response}`,
-          input: event,
+          message: `Service:  ${name}, Status: ${response.status}`,
+          content: response.result,
         },
-        null,
-        2
       ),
     };
   } catch (error) {
